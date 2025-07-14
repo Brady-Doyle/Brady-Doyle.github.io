6495af18e890d16c3b66197595fc26e1c8e33034
Brady-Doyle <brady_120@outlook.com>
2025-07-13 20:06:35 -0700

    back to top officially fixed

--- js/main.js
+++ js/main.js
@@ -8,6 +8,9 @@ window.addEventListener("DOMContentLoaded", function () {
     const dropdown_title=document.getElementById("dropdown_title")
     const dropdown_wrapper=document.getElementById("dropdown_wrapper")
 
+    // Back to Top
+    const back_to_top=this.document.getElementById("back_to_top")
+
     // Light/Dark
     switch_button.addEventListener("click", function() {
         if (body.style.color==="black" && body.style.background==="white") {
@@ -59,4 +62,16 @@ window.addEventListener("DOMContentLoaded", function () {
     dropdown_title.addEventListener("click", dropdown_open);
     dropdown_title.addEventListener("mouseenter", dropdown_open);
     dropdown_wrapper.addEventListener("mouseleave", dropdown_close);
+});
+
+// Back to Top
+window.addEventListener("scroll", ()=> {
+    if (window.scrollY>=100) {
+        back_to_top.style.opacity="1";
+        back_to_top.style.pointerEvents="auto";
+    }
+    else {
+        back_to_top.style.opacity="0";
+        back_to_top.style.pointerEvents="none";
+    }
 });
