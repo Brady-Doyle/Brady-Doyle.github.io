59f188380d2676db0723d017132004a0449bd863
Brady-Doyle <148658129+Brady-Doyle@users.noreply.github.com>
2025-07-17 16:25:03 -0700

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -1,4 +1,7 @@
+// Loading themes
 const theme=localStorage.getItem("theme")
+
+// Loading themes
 if (theme==="dark") {
   document.documentElement.style.background="black";
   document.documentElement.style.color="white";
